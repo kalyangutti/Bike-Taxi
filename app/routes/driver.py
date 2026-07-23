24c6b2b59d8c04ac40a521a0266b8a0516e1787d
kalyangutti <kalyangutti19@gmail.com>
2026-07-24 01:18:11 +0530

    Level 2: Implement User Module with search, filter, sorting and pagination

--- app/routes/driver.py
+++ app/routes/driver.py
@@ -1,79 +1,149 @@
 from fastapi import APIRouter, HTTPException, status
 
+from app.schemas.driver import DriverCreate, DriverRead, DriverUpdate
+
 router = APIRouter(prefix="/drivers", tags=["Driver"])
 
 database = {
-    1: {"name": "Rahul", "phone": "9876543210", "vehicle_number": "TN01AB1234"},
-    2: {"name": "Arjun", "phone": "9123456789", "vehicle_number": "TN10CD5678"},
-    3: {"name": "Kiran", "phone": "9988776655", "vehicle_number": "KA05EF4321"},
-    4: {"name": "Sai", "phone": "9090909090", "vehicle_number": "AP16GH7890"},
-    5: {"name": "Charan", "phone": "9012345678", "vehicle_number": "TS09JK1122"},
+    1: {
+        "name": "Rahul Sharma",
+        "email": "rahul@gmail.com",
+        "phone": "9876543210",
+        "age": 24,
+        "vehicle_number": "TN01AB1234",
+        "password": "Rahul@123",
+        "gender": "MALE",
+        "is_active": True,
+        "rides": 125,
+    },
+    2: {
+        "name": "Arjun Kumar",
+        "email": "arjun@gmail.com",
+        "phone": "9123456789",
+        "age": 27,
+        "vehicle_number": "TN10CD5678",
+        "password": "Arjun@123",
+        "gender": "MALE",
+        "is_active": True,
+        "rides": 210,
+    },
+    3: {
+        "name": "Kiran Reddy",
+        "email": "kiran@gmail.com",
+        "phone": "9988776655",
+        "age": 29,
+        "vehicle_number": "KA05EF4321",
+        "password": "Kiran@123",
+        "gender": "MALE",
+        "is_active": False,
+        "rides": 98,
+    },
+    4: {
+        "name": "Sai Teja",
+        "email": "sai@gmail.com",
+        "phone": "9090909090",
+        "age": 23,
+        "vehicle_number": "AP16GH7890",
+        "password": "Sai@1234",
+        "gender": "MALE",
+        "is_active": True,
+        "rides": 56,
+    },
+    5: {
+        "name": "Charan R",
+        "email": "charan@gmail.com",
+        "phone": "9012345678",
+        "age": 26,
+        "vehicle_number": "TS09JK1122",
+        "password": "Charan@123",
+        "gender": "MALE",
+        "is_active": True,
+        "rides": 175,
+    },
 }
 
 
-# Get all drivers
-@router.get("/")
-def get_all_drivers():
-    return database
-
-
-# Get driver by ID
-@router.get("/{id}")
+# Get Drivers
+@router.get("/id", response_model=DriverRead)
 def get_driver(id: int):
     if id not in database:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Driver not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"ID {id} not found"
         )
     return database[id]
 
 
-# Create driver
-@router.post("/", status_code=status.HTTP_201_CREATED)
-def create_driver(name: str, phone: str, vehicle_number: str):
-    new_id = max(database.keys(), default=0) + 1
-
-    database[new_id] = {
-        "name": name,
-        "phone": phone,
-        "vehicle_number": vehicle_number,
-    }
-
-    return {"id": new_id, "driver": database[new_id]}
+# Get all Drivers
+@router.get("/alldrivers")
+def get_all_drivers():
+    return database
 
 
-# Update driver
-@router.patch("/{id}")
-def update_driver(
-    id: int,
-    name: str | None = None,
-    phone: str | None = None,
-    vehicle_number: str | None = None,
-):
+# Search User
+@router.get("/filter")
+def search_user(field: str, value: str):
+    result = []
+
+    for driver in database.values():
+        if str(driver.get(field)) == value:
+            result.append(driver)
+
+    return result
+
+
+# Create  Driver
+@router.post("/create")
+def create_driver(body: DriverCreate):
+    new_id = max(database.keys()) + 1
+    database[new_id] = {**body.model_dump()}
+    return {"Detail": f"New Id {new_id} has been created"}
+
+
+# Update Driver Details
+@router.patch("/updatedetails")
+def update_driver_details(driver: DriverUpdate, driver_id: int):
+
+    print("Before:", database[driver_id])
+
+    details = driver.model_dump(exclude_unset=True)
+    print("Received:", details)
+
+    database[driver_id].update(details)
+
+    print("After:", database[driver_id])
+
+    return {"message": "Updated", "driver": database[driver_id]}
+
+
+# Sorting
+@router.get("/sort")
+def sort_drivers(field: str, order: str = "asc"):
+
+    drivers = list(database.values())
+
+    reverse = order.lower() == "desc"
+
+    sorted_drivers = sorted(drivers, key=lambda driver: driver[field], reverse=reverse)
+
+    return sorted_drivers
+
+
+# Pagination
+@router.get("/offset")
+def paginate_drivers(page: int , size: int ):
+    drivers = list(database.values())
+    start = (page - 1) * size
+    end = start + size
+    return drivers[start:end]
+
+
+# Delete Driver Details
+@router.delete("/id")
+def deleteDetails(id: int):
     if id not in database:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Driver not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"{id} not Found in the Database",
         )
-
-    if name is not None:
-        database[id]["name"] = name
-
-    if phone is not None:
-        database[id]["phone"] = phone
-
-    if vehicle_number is not None:
-        database[id]["vehicle_number"] = vehicle_number
-
-    return {"message": "Driver updated successfully", "driver": database[id]}
-
-
-# Delete driver
-@router.delete("/{id}")
-def delete_driver(id: int):
-    if id not in database:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Driver not found"
-        )
-
-    deleted_driver = database.pop(id)
-
-    return {"message": "Driver deleted successfully", "driver": deleted_driver}
+    del database[id]
+    return {"detail": f"Id of {id} database has been deleted"}
---
+++ app/schemas/driver.py
@@ -0,0 +1,80 @@
+import re
+from enum import Enum
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, Field, StringConstraints, field_validator
+
+
+class Gender(str, Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    OTHER = "OTHER"
+
+
+PhoneNumber = Annotated[
+    str,
+    StringConstraints(
+        pattern=r"^[6-9]\d{9}$",
+        strip_whitespace=True,
+    ),
+]
+
+
+def validate_password( value) -> str:
+    if len(value) < 8:
+        raise ValueError("Password must be at least 8 characters long.")
+
+    if not re.search(r"[A-Z]", value):
+        raise ValueError("Password must contain at least one uppercase letter.")
+
+    if not re.search(r"[a-z]", value):
+        raise ValueError("Password must contain at least one lowercase letter.")
+
+    if not re.search(r"\d", value):
+        raise ValueError("Password must contain at least one digit.")
+
+    if not re.search(r"[!@#$%^&*()~{}\[\]\";<>?/]", value):
+        raise ValueError("Password must contain at least one special character.")
+
+    return value
+
+
+class BaseClass(BaseModel):
+    name: str
+    email: EmailStr
+    phone: PhoneNumber
+    age: int = Field(ge=18)
+    vehicle_number: str
+
+
+class BaseDriver(BaseClass):
+    password: str
+    gender: Gender
+    is_active: bool = Field(default=True)
+    rides: int = Field(default=0, ge=0)
+
+    @field_validator("password")
+    @classmethod
+    def password_validtor(cls, value) -> str:
+        return validate_password(value)
+
+
+class DriverRead(BaseClass):
+    pass
+
+
+class DriverCreate(BaseDriver):
+    pass
+
+
+class DriverUpdate(BaseModel):
+    phone: PhoneNumber | None = None
+    vehicle_number: str | None = None
+    password: str | None = None
+
+    @field_validator("password")
+    @classmethod
+    def password_validator(cls, value):
+        if value is None:
+            return None
+        return validate_password(value)
